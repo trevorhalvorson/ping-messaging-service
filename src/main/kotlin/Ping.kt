8672243f6eb3c89b0534d4f03ccc7d7d56755639
Trevor Halvorson <trevor.halvorson@gmail.com>
2017-09-21 00:20:03 -0700

    Clean up files

--- src/main/kotlin/Ping.kt
+++ src/main/kotlin/Ping.kt
@@ -1,22 +1,15 @@
-import com.google.gson.Gson
-import org.apache.commons.mail.DefaultAuthenticator
-import org.apache.commons.mail.EmailException
-import org.apache.commons.mail.SimpleEmail
 import org.jetbrains.ktor.application.Application
 import org.jetbrains.ktor.application.install
-import org.jetbrains.ktor.application.log
 import org.jetbrains.ktor.features.CallLogging
 import org.jetbrains.ktor.features.DefaultHeaders
 import org.jetbrains.ktor.gson.GsonSupport
 import org.jetbrains.ktor.locations.Locations
 import org.jetbrains.ktor.locations.post
 import org.jetbrains.ktor.locations.location
-import org.jetbrains.ktor.request.receive
-import org.jetbrains.ktor.response.respond
 import org.jetbrains.ktor.routing.Routing
 
 @location("/")
-class index()
+class index
 
 fun Application.main() {
     install(DefaultHeaders)
@@ -25,26 +18,6 @@ fun Application.main() {
     install(GsonSupport)
     install(Routing) {
         post<index> {
-
-            val gson = Gson()
-            val req = gson.fromJson(call.receive<String>(), Request::class.java)
-
-            val email = SimpleEmail()
-            email.hostName = "smtp.googleemail.com"
-            email.setSmtpPort(465)
-            email.setAuthenticator(DefaultAuthenticator("useranme", "password"))
-            email.isSSLOnConnect = true
-            email.subject = "Your Custom APK"
-            email.setMsg("Your custom build APK is attached to this email")
-            email.addTo(req.email)
-
-            try {
-                email.send()
-            } catch (e: EmailException) {
-                log.debug("Failed to send email: ${e.message}")
-            } finally {
-                call.respond(req)
-            }
 
         }
     }
